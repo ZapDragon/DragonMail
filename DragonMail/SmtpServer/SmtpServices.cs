19c41b15d6de5851b13ac77887081ca6abe9b46b
XylexRayne <17253171+XylexRayne@users.noreply.github.com>
2017-10-01 07:02:53 -0700

    Completed Smtp Client Object Handling.

--- DragonMail/SmtpServer/SmtpServices.cs
+++ DragonMail/SmtpServer/SmtpServices.cs
@@ -11,29 +11,76 @@ namespace DragonMail
     class SmtpServices
     {
         public static List<SmtpServer> SmtpServers = new List<SmtpServer>();
+        public static Dictionary<TcpClient, SmtpClient> SmtpClients = new Dictionary<TcpClient, SmtpClient>();
 
         public static void SmtpMain()
         {
-            foreach (int Port in MainClass.Listeners)
-            {
-                SmtpServers.Add(new SmtpServer(Port));
+            // Create a new Smtp Listener object for ever listener port specified, and start the service.
+            foreach (int Port in MainClass.Listeners) {
+                SmtpServer SmtpService = new SmtpServer(Port);
+                SmtpServers.Add(SmtpService);
+                SmtpService.SmtpListener.Start();
+                    
             }
 
-            Console.ReadKey();
+            // Begining Main service loop.
+            while (true)
+            {
+                
+                // Check Each SmtpServer object Listener for new connections and accept them.
+                foreach (SmtpServer Listener in SmtpServers)
+                {
+                    if (Listener.SmtpListener.Pending()) { newSmtpClient(Listener.SmtpListener.AcceptTcpClient()); }
+                }
+
+            }
 
 
         }
- 
+
+        public static void newSmtpClient(TcpClient client)
+        {
+            string address = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
+
+            SmtpClient clientObject = new SmtpClient(client);
+            SmtpClients.Add(client, clientObject);
+
+            clientObject.ConnectingIP = address;
+            clientObject.CurrentMode = "Login";
+            Task.Factory.StartNew(() => Write(client, "220 " + MainClass.PrimaryDomain + Environment.NewLine));
+        }
+
+
+        public static async Task Write(TcpClient client, string Data)
+        {
+            byte[] packet = Encoding.UTF8.GetBytes(Data);
+            await client.GetStream().WriteAsync(packet, 0, packet.Length);
+        }
+
+
+
     }
 
     public class SmtpServer
     {
-        TcpListener SmtpListener;
+        public TcpListener SmtpListener;
         public SmtpServer(int Port)
         {
             SmtpListener = new TcpListener(IPAddress.Parse(MainClass.ListenIP), Port);
             Console.WriteLine("Smtp Service Started on Port " + Port);
         }
 
+    }
+
+    public class SmtpClient
+    {
+        public TcpClient TcpClient;
+        public string ConnectingIP;
+        public string CurrentMode;
+        public SmtpClient(TcpClient client)
+        {
+            TcpClient = client;
+            Console.WriteLine("New Smtp Client.");
+        }
     }
 }
